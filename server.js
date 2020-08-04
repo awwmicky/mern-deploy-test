07c51e70e876edfe06d60307cd8766e30a8a9533
TS22082 <ts22082@gmail.com>
2020-08-04 15:21:31 -0700

    setup for send all client files

--- server.js
+++ server.js
@@ -1,41 +1,42 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-require('dotenv').config()
+require("dotenv").config();
 
 /* Middleware */
-if (process.env.NODE_ENV === 'production') {
-    app.use( express.static('../client/build/') )
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("../client/build/"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
-app.use( express.json() )
-app.use( express.urlencoded({ extended : true }) )
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 /* API */
-const apiRoutes = require('./routes/api-route.js');
-app.use('/api', apiRoutes)
+const apiRoutes = require("./routes/api-route.js");
+app.use("/api", apiRoutes);
 
 /* Error Handling */
-const errors = require('./middleware/errors.js');
-app.use( errors.pageNotFoundError )
-app.use( errors.internalServerError )
-
+const errors = require("./middleware/errors.js");
+app.use(errors.pageNotFoundError);
+app.use(errors.internalServerError);
 
 /* MongoDB */
-const {  MONGO_URI , options } = require('./config/keys.js');
-const mongoose = require('mongoose');
+const { MONGO_URI, options } = require("./config/keys.js");
+const mongoose = require("mongoose");
 const connection = mongoose.connection;
 const PORT = process.env.PORT || 5000;
 
 connection
-.once('open', _ => console.log('MongoDB Connected — √', '\n' + MONGO_URI) )
-.on('error', err => console.log('Connection Error — ×', '\n' + err) )
+  .once("open", (_) => console.log("MongoDB Connected — √", "\n" + MONGO_URI))
+  .on("error", (err) => console.log("Connection Error — ×", "\n" + err));
 
 /* Connection */
-mongoose.connect(MONGO_URI, options, err => {
-    if (err) throw err;
+mongoose.connect(MONGO_URI, options, (err) => {
+  if (err) throw err;
 
-    app.listen(PORT, () => { 
-        console.log(`Test Server - http://localhost:${PORT}`) 
-    })
-})
+  app.listen(PORT, () => {
+    console.log(`Test Server - http://localhost:${PORT}`);
+  });
+});
