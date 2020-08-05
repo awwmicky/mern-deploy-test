1e4a48f5f1b0a226864c27a0eeb022642d0b19d6
awwmicky <michael_fred_alvarez@yahoo.com>
2020-08-05 01:00:40 -0700

    middleware adjustment

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
 const express = require("express");
 const app = express();
-require("dotenv").config();
+require("dotenv").config()
 
 /* Middleware */
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../client/build/"));
+  app.use(express.static("./client/build/"))
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  })
 }
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 /* API */
 const apiRoutes = require("./routes/api-route.js");
-app.use("/api", apiRoutes);
+app.use("/api", apiRoutes)
 
 /* Error Handling */
 const errors = require("./middleware/errors.js");
-app.use(errors.pageNotFoundError);
-app.use(errors.internalServerError);
+app.use(errors.pageNotFoundError)
+app.use(errors.internalServerError)
 
 /* MongoDB */
 const { MONGO_URI, options } = require("./config/keys.js");
@@ -29,8 +29,8 @@ const connection = mongoose.connection;
 const PORT = process.env.PORT || 5000;
 
 connection
-  .once("open", (_) => console.log("MongoDB Connected — √", "\n" + MONGO_URI))
-  .on("error", (err) => console.log("Connection Error — ×", "\n" + err));
+.once("open", (_) => console.log("MongoDB Connected — √", "\n" + MONGO_URI))
+.on("error", (err) => console.log("Connection Error — ×", "\n" + err))
 
 /* Connection */
 mongoose.connect(MONGO_URI, options, (err) => {
@@ -41,4 +41,4 @@ mongoose.connect(MONGO_URI, options, (err) => {
   });
 });
 
-// "proxy": "http://localhost:5000"
+// React → "proxy": "http://localhost:5000"
